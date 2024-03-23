8aa857d45eb7d324979965c6bbabdb67c60959c1
Abdul-Haq <159776568+karachiboy@users.noreply.github.com>
2024-03-23 19:30:07 +0500

    Created a Cli guessing number Game

---
+++ index.ts
@@ -0,0 +1,18 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+const randomNumber = Math.floor(Math.random() * 10);
+
+const answer = await inquirer.prompt([
+  {
+    name: "GuessedNumber",
+    type: "number",
+    message: "Guess a Number between 0-10",
+  },
+]);
+
+if(answer.GuessedNumber === randomNumber) {
+    console.log("Congratulation you Guessed a right Number ;)");
+} else console.log("You guess a wrong Number :( ");
+
